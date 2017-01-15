e95c140e8b9b8e802c9174d52e7e87d38ca7941b
Donovan Solms <donovan.solms@gmail.com>
2017-01-15 21:07:27 +0200

    Implementing interface

--- LocalInteractionEngine/LocalInteractionEngine.cs
+++ LocalInteractionEngine/LocalInteractionEngine.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
 
-namespace LocalInteractionEngine
+using System;
+
+using Limitless.Runtime.Types;
+using Limitless.Runtime.Interfaces;
+
+namespace Limitless.LocalInteractionEngine
 {
-    public class LocalInteractionEngine
+    /// <summary>
+    /// An Interaction Engine that only operates locally.
+    /// </summary>
+    public class LocalInteractionEngine : IModule, IInteractionEngine
     {
+        public void Configure(dynamic settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeregisterSkill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetConfigurationType()
+        {
+            return typeof(LocalInteractionEngineConfig);
+        }
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ListSkills()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOData ProcessInput(IOData ioData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSkill()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
